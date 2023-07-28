a1dc2c3f3171fbd097e4ebeaeda1a1ee4699c8a4
Om Singh <94895043+omsinghthakur@users.noreply.github.com>
2023-07-28 12:54:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -14,13 +14,13 @@ class Animal {
 
 class Dog extends Animal {
 	 bark() {
-    console.log("The Golden Retriever makes a sound"); 
+    console.log("woof"); 
   } 
 }
 
 class Cat extends Animal {
 	 purr() {
-    console.log("The Siamese makes a sound");
+    console.log("purr");
   } 
 }  
 // Do not change the code below this line
